f9cf7bdd7584e1ef8e9f8224f4df1d2b8658623b
Daniell-Sparta-Bow <dbow@spartaglobal.com>
2024-04-15 14:20:07 +0100

    Added and Refactored Program Class

---
+++ Week2/RefactoringAndUnitTests/src/main/java/com/sparta/db/Program.java
@@ -0,0 +1,18 @@
+package com.sparta.db;
+
+public class Program {
+    public static void main(String[] args) {
+        int timeOfDay = 21;
+        String greeting = getGreeting(timeOfDay);
+    }
+
+    private static String getGreeting(int timeOfDay) {
+        if (timeOfDay >= 5 && timeOfDay <= 12) {
+            return "Good morning!";
+        } else if (timeOfDay >= 12 && timeOfDay <= 18) {
+            return "Good afternoon!";
+        } else {
+            return "Good evening!";
+        }
+    }
+}
